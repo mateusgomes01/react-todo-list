90d4f859be7f10b39e38a696f086dc5b3a00d2b3
mateusgomes01 <mateus.melo@liferay.com>
2022-01-22 22:09:30 -0300

    Static version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ItemRow({ description }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="checkbox"/>
+      <span>{ description }</span>
     </div>
-  );
+  )
 }
+
+function ItemList({ items }) {
+  const rows = []
+
+  items.forEach((item) => {
+    rows.push(
+      <ItemRow
+        description={item.description}
+      />
+    )
+  })
+
+  return (
+    <div>
+      { rows }
+    </div>
+  )
+}
+
+function AddItem() {
+  return (
+    <form>
+      <input type="text" placeholder="Ad"></input>
+      <button>Add</button>
+    </form>
+  )
+}
+
+function TodoList({ items }) {
+  return (
+    <div>
+      <h1>Mateus' ToDo List</h1>
+      <AddItem/>
+      <ItemList items={ items } />
+    </div>
+  )
+}
+
+function App() {
+  return <TodoList items={TODO_LIST}/>;
+}
+
+const TODO_LIST = [
+  {description: "Memorize Psalms 147"},
+  {description: "Do laundry"},
+  {description: "Study HTML CORS"},
+  {description: "Study JS"},
+  {description: "Study React"},
+];
 
 export default App;
