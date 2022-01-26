991c6f5a3f7999ce7a22ccc835badb7a28ac6288
mateusgomes01 <mateus.melo@liferay.com>
2022-01-26 06:46:58 -0300

    Adds state

--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-import './App.css';
+import { useState } from "react";
 
 function ItemRow({ description }) {
   return (
     <div>
-      <input type="checkbox"/>
-      <span>{ description }</span>
+      <input type="checkbox" />
+      <span>{` ${description}`}</span>
     </div>
-  )
+  );
 }
 
-function ItemList({ items }) {
-  const rows = []
+function ItemList({ itemList }) {
+  const rows = [];
 
-  items.forEach((item) => {
-    rows.push(
-      <ItemRow
-        description={item.description}
-      />
-    )
-  })
+  itemList.forEach((item) => {
+    rows.push(<ItemRow description={item.description} />);
+  });
 
-  return (
-    <div>
-      { rows }
-    </div>
-  )
+  return <div>{rows}</div>;
 }
 
-function AddItem() {
+function AddItem({ currentItem, onNewItemChange, addNewItem }) {
   return (
     <form>
-      <input type="text" placeholder="Ad"></input>
-      <button>Add</button>
+      <input
+        type="text"
+        value={currentItem}
+        placeholder="Ad..."
+        onChange={(e) => {
+          onNewItemChange(e.target.value);
+        }}
+      />
+      <button onClick={addNewItem}>Add</button>
     </form>
-  )
+  );
 }
 
 function TodoList({ items }) {
+  const [itemList, setItemList] = useState(items);
+  const [newItem, setNewItem] = useState("");
+
+  function addNewItem() {
+    setItemList([...itemList, { description: newItem }]);
+  }
+
   return (
-    <div>
+    <div /*className="todo-list-box"*/>
       <h1>Mateus' ToDo List</h1>
-      <AddItem/>
-      <ItemList items={ items } />
+      <AddItem
+        currentItem={newItem}
+        onNewItemChange={setNewItem}
+        addNewItem={addNewItem}
+      />
+      <ItemList itemList={itemList} />
     </div>
-  )
+  );
 }
 
 function App() {
-  return <TodoList items={TODO_LIST}/>;
+  return <TodoList items={TODO_LIST} />;
 }
 
 const TODO_LIST = [
-  {description: "Memorize Psalms 147"},
-  {description: "Do laundry"},
-  {description: "Study HTML CORS"},
-  {description: "Study JS"},
-  {description: "Study React"},
+  { description: "Memorize Psalms 147" },
+  { description: "Do laundry" },
+  { description: "Study HTML CORS" },
+  { description: "Study JS" },
+  { description: "Study React" }
 ];
 
 export default App;
